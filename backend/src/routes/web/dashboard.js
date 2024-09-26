45cfe036a87719ff4a722948453b660064c16c2a
jeancharlesx64 <jean.albuquerque@sptech.school>
2024-09-26 11:55:49 -0300

    [FIX] :: Fixing the security - Session storage was not working

--- backend/src/routes/web/dashboard.js
+++ backend/src/routes/web/dashboard.js
@@ -6,32 +6,7 @@ const fazendaController = require('../../controllers/fazendaController');
 // Rota raiz dentro do grupo /dashboard
 router.get('/', async function(req, res) {
     if (req.session.authenticated) {
-
-        // dados do usuário
-        const user = req.session.user;
-
-        let fazendasResponse;
-        let fazendas;
-
-        fazendasResponse = await userController.getFazendas(user.session_userId);
-        fazendas = fazendasResponse.bd_fazendas;
-
-        let countFazendaHec = await fazendaController.countFazendaHec(user.session_userId);
-        let countSensors = await fazendaController.countSensors(user.session_userId);
-
-    
- 
-        res.render('dashboard', {
-            userId: user.session_userId,
-            userName: user.session_userName,
-            userEmail: user.session_userEmail,
-            userAdmin: user.session_userAdmin,
-            userCompany: user.session_userCompany,
-            userFazenda: user.session_userFazenda,
-            fazendas: fazendas,
-            countFazendaHec: countFazendaHec,
-            countSensors: countSensors,
-        });
+        res.render('dashboard');
 
     }else{
         req.session.hasError = true;
